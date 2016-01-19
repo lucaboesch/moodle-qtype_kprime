ccdf2cfd39335d442ceb6de40f07bfb8faa3b2f0
ahourani <amr.hourani@id.ethz.ch>
2016-01-19 10:04:14 +0100

    migrate matrix to kprime

--- bin/mig_matrix_to_kprime.php
+++ bin/mig_matrix_to_kprime.php
@@ -16,8 +16,8 @@
 /**
  *
  * @package qtype_kprime
- * @author Juergen Zimmer jzimmer1000@gmail.com
- * @copyright eDaktik 2014 andreas.hruska@edaktik.at
+ * @author Amr Hourani amr.hourani@id.ethz.ch
+ * @copyright ETHz 2016 amr.hourani@id.ethz.ch
  */
 require_once (dirname(__FILE__) . '/../../../../config.php');
 require_once ($CFG->dirroot . "/lib/moodlelib.php");
