5282915971ade2ce88dff1e98dc63f1e391383d5
ahourani <amr.hourani@id.ethz.ch>
2016-05-30 15:15:08 +0200

    update the release date

--- version.php
+++ version.php
@@ -22,8 +22,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_kprime';
-$plugin->version = 2016051200;
+$plugin->version = 2016053000;
 $plugin->requires = 2013111904; // Moodle >=2.6.4.
 
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.6 for Moodle 2.6+';
+$plugin->release = '1.3 for Moodle 2.6+';
