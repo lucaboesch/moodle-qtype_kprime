f4de8f4c3980c678a0f9b5f8e7a4d45bc1949956
Amr <nexterday@users.noreply.github.com>
2017-10-20 15:04:22 +0200

    fixes #20
    updates to accept final grade for interactive behaviour as non zero (to avoid inf fraction)

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_kprime';
-$plugin->version = 2017090600;
+$plugin->version = 2017102000;
 $plugin->requires = 2013111904; // Moodle >=2.6.4.
 
 $plugin->maturity = MATURITY_STABLE;
