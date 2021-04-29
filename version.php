f304bf4575a88175684c87a83d8a90d4ad2d24b6
Martin Hanusch <martin.hanusch@let.ethz.ch>
2021-04-29 09:29:06 +0200

    Updating version for last commit (9be0563)

--- version.php
+++ version.php
@@ -28,7 +28,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qtype_kprime';
-$plugin->version = 2021041500;
+$plugin->version = 2021042900;
 $plugin->requires = 2013111904; // Moodle >=2.6.4.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.3 for Moodle 3.4+';
