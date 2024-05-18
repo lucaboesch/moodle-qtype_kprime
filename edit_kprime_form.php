a4ef8d0edc3e57f94da57a3749998fbd9af726df
Nexterday <nexterday@users.noreply.github.com>
2024-05-18 16:54:57 +0200

    Update edit_kprime_form.php

--- edit_kprime_form.php
+++ edit_kprime_form.php
@@ -158,7 +158,7 @@ class qtype_kprime_edit_form extends question_edit_form {
         $this->definition_inner($mform);
         $this->add_interactive_settings();
 
-        if (core_tag_tag::is_enabled('core_question', 'question') && class_exists('qbank_tagquestion\\tags_action_column') &&
+        if (core_tag_tag::is_enabled('core_question', 'question') &&
              \core\plugininfo\qbank::is_plugin_enabled('qbank_tagquestion')) {
             $this->add_tag_fields($mform);
         }
